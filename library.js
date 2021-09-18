6e613987f91b7f3e1f8a3728505e3cc2af741d2d
erlend <ekrogstad@gmail.com>
2021-09-18 13:31:18 +0200

    created some new classes but broke some functions

--- library.js
+++ library.js
@@ -56,16 +56,13 @@ class Library {
         this.books = books
     }
     logBooks() {
-        this.books.forEach(boook => console.log(book))
+        this.books.forEach(book => console.log(book))
     }
-    addBookToLibrary(book) {
-        this.books.push(book)
+    addBookToLibrary(newBook) {
+        console.log(newBook)
+        this.books.push(newBook)
     }
-    refreshID() {
-        document.querySelectorAll(".card").forEach((div, i) => {
-            div.setAttribute("delete-id", i )
-        })
-    }
+    
 }
 
 class Book {
@@ -80,20 +77,21 @@ class Book {
     }
 }
 
-let myLibrary = new Library([
+let myLibrary = new Library(
     new Book("Sømannen", "Jo Nesbø", 251, false),
     new Book("Rødstrupe", "Jo Nesbø", 492, true),
     new Book("Berlinerpopplene", "Anne B. Ragde", 379, true),
     new Book("Lord of the Rings", "J. R. R. Tolkien", 150, true),
     new Book("Donald Duck", "Don Rosa", 1211, true)
-] )
+)
 
 function setLibrary() {
     localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
 } 
 function getLibrary(){
     let loadedLibrary = JSON.parse(localStorage.getItem("myLibrary"))
-    loadedLibrary.forEach(book => book.__proto__ = new Book())
+    loadedLibrary.__proto__ = new Library()
+    loadedLibrary.books.forEach(book => Object.setPrototypeOf(book, new Book) )
     return loadedLibrary
 }
 
@@ -110,124 +108,115 @@ if(!localStorage.getItem("myLibrary")) {
 
 
 
-// class UI {
-//     constructor()
-// }
+class UI {
 
-function randomColor() {
-    return randomNumber = Math.floor(Math.random() * 255)
     
-}
+    static container = () => document.querySelector(".container")
 
+    static randomColor() {
+        return Math.floor(Math.random() * 255)   
+    }
 
-const body = document.querySelector("body")
-const container = document.querySelector(".container")
-
-function createBookCard(titleBook, authorBook, pagesBook, readBook) {
-    const div = document.createElement("div")
-    div.classList.add("card")
-    const title = document.createElement("h2")
-    title.classList.add("title")
-    title.innerText = titleBook
-    const author = document.createElement("h3")
-    author.classList.add("author")
-    author.innerText = authorBook
-    const pages = document.createElement("h4")
-    pages.classList.add("pages")
-    pages.innerText = pagesBook + " pages"
-    const read = document.createElement("h4")
-    read.classList.add("read")
-    read.innerText = readBook ? "Book is: read" : "Book is: Not read"
-
-    const topDiv = document.createElement("div")
-    topDiv.id = "topDiv"
-
-    const delBtn = document.createElement("div")
-    delBtn.id = "delBtn"
-    delBtn.textContent = "x"
-    div.setAttribute("delete-id", myLibrary.findIndex(book => book.title == titleBook)) 
-    topDiv.appendChild(delBtn)
-    
-    const readBtn = document.createElement("div")
-    readBtn.id = "readBtn"
-    readBtn.textContent = readBook ? "Read" : "Not read";
-    readBtn.setAttribute("read-id", myLibrary.findIndex(book => book.title == titleBook)) 
-
-    topDiv.appendChild(readBtn)
-    topDiv.appendChild(delBtn)
-    
-    
-    div.append(topDiv)
-
-    //delete book
-    delBtn.addEventListener("click", e => {
-        console.log(e.target.parentElement.parentElement.attributes["delete-id"].value)
-        let index = e.target.parentElement.parentElement.attributes["delete-id"].value
-        myLibrary.splice(index, 1)
-        setLibrary()
-        e.target.parentElement.parentElement.remove()
-        let leftOverDivs = e.target.parentElement.parentElement
-        refreshID()
-        
+    static refreshID() {
+        document.querySelectorAll(".card").forEach((div, i) => {
+            div.setAttribute("delete-id", i )
         })
-    //togle read
-    readBtn.addEventListener("click", (e) => {
-        // let title = e.target.parentElement.parentElement.childNodes[1].textContent
-        let index = e.target.parentElement.parentElement.attributes["delete-id"].value
-        // myLibrary[index].read = !myLibrary[index].read
-        myLibrary[index].toggleRead();
-        read.innerText  =  `Book is:  ${myLibrary[index].read ? "Read" : "Not Read"}`
-        setLibrary()
-        readBtn.textContent = myLibrary[index].read ? "Read" : "Not Read"
-        
-
-    })
-
-
-    div.append(title, author, pages, read)
-    div.style.backgroundColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()}, 1)`
-
-    container.appendChild(div)
-
-}
-function printLibrary() {myLibrary.forEach(book => console.log(book.title))}
-
-function displayAllBooks() {
-    container.textContent = ""
-    myLibrary.forEach(book => {
-        createBookCard(book.title, book.author, book.pages, book.read)
-        
-    })
-}
-
-const formBtn = document.querySelector("#form-btn")
-const form = document.querySelector("form")
-form.style.display = "none"
-formBtn.addEventListener("click", ()=> {
-    form.style.display == "none" ? form.style.display = "block" : form.style.display = "none"
-})
-
-
-
-
-
-const submit = document.querySelector("input[type='submit']")
-submit.addEventListener("click", e => {
-            e.preventDefault()
-
-            let form = e.target.parentElement
-            let title = form[0].value
-            let author = form[1].value
-            let pages =  form[2].value
-            let reada =  form[3].checked
-            let book = new Book(title, author, pages, reada)
-            addBookToLibrary(book)
-            setLibrary()
-            createBookCard(book.title, book.author, book.pages, book.read)
-            refreshID()
+    }
+    
+    static displayAllBooks() {
+        this.container().textContent = ""
+        myLibrary.books.forEach(book => {
+            this.createBookCard(book.title, book.author, book.pages, book.read)
             
-            //createBookCard(book.title, book.author, book.pages, book.read)
         })
+    }
+
+    static createBookCard(title, author, pages, read) {
+        function createElementAddClassAddContent(element, className, content) {
+            let temp = document.createElement(element)
+            temp.classList.add(className)
+            if(content) temp.innerText = content
+            return temp
+        }
+        function createButtons(id, content) {
+            let temp = document.createElement("div")
+            temp.id = id
+            temp.innerText = content
+            temp.setAttribute("data-id", myLibrary.books.findIndex(book => book.title == title))
+            return temp
+        }
+        const divElement = createElementAddClassAddContent("div", "card")
+        const titleElement = createElementAddClassAddContent("h2", "title", title)
+        const authorElement = createElementAddClassAddContent("h3", "author", author)
+        const pagesElement = createElementAddClassAddContent("h4", "pages", pages + " pages")
+        const readElement = createElementAddClassAddContent("h4", "read", read ? "Book is: Read" : "Book is: Not read")
+
+        const topDiv = document.createElement("div")
+        topDiv.id = "topDiv"
+
+        const deleteButton  = createButtons("delete-btn", " X")
+        // const delBtn = document.createElement("div")
+        // delBtn.id = "delBtn"
+        // delBtn.textContent = "x"
+        // div.setAttribute("delete-id", myLibrary.findIndex(book => book.title == titleBook)) 
+        
+        const readButton = createButtons("read-btn", read ? "Read" : "Not Read")
+        // const readBtn = document.createElement("div")
+        // readBtn.id = "readBtn"
+        // readBtn.textContent = readBook ? "Read" : "Not read";
+        // readBtn.setAttribute("read-id", myLibrary.findIndex(book => book.title == titleBook)) 
+        //console.log(readButton)
+        topDiv.appendChild(readButton)
+        topDiv.appendChild(deleteButton)
+        divElement.append(topDiv)
+
+        deleteButton.addEventListener("click", e => {
+            let index = e.target.attributes["data-id"].value
+            myLibrary.books.splice(index, 1)
+            setLibrary()
+            e.target.parentElement.parentElement.remove()
+            this.refreshID()
+        })
+        readButton.addEventListener("click", (e) => {
+            let index = e.target.attributes["data-id"].value
+            myLibrary.books[index].toggleRead();
+            readElement.innerText  =  `Book is:  ${myLibrary.books[index].read ? "Read" : "Not Read"}`
+            setLibrary()
+            readButton.textContent = myLibrary.books[index].read ? "Read" : "Not Read"
+        })
+        divElement.append(titleElement, authorElement, pagesElement, readElement)
+        divElement.style.backgroundColor = `rgba(${UI.randomColor()}, ${UI.randomColor()}, ${UI.randomColor()}, 1)`
+        this.container().appendChild(divElement)
+
+
+    }
+
+    static setUpForm() {
+        const formBtn = document.querySelector("#form-btn")
+        const form = document.querySelector("form")
+        form.style.display = "none"
+        formBtn.addEventListener("click", ()=> {
+            form.style.display == "none" ? form.style.display = "block" : form.style.display = "none"
+        })
+
+        const submit = document.querySelector("input[type='submit']")
+        submit.addEventListener("click", e => {
+                    e.preventDefault()
+
+                    let form = e.target.parentElement
+                    let title = form[0].value
+                    let author = form[1].value
+                    let pages =  form[2].value
+                    let reada =  form[3].checked
+                    let book = new Book(title, author, pages, reada)
+                    myLibrary.addBookToLibrary(book)
+                    setLibrary()
+                    this.createBookCard(book.title, book.author, book.pages, book.read)
+                    this.refreshID()
+                })
+    }
+}
+
 
 
 // Stupid me thought i had to generate form using JS. New approach is to make in HTML and hide it. Bring it out with JKS
@@ -281,8 +270,8 @@ submit.addEventListener("click", e => {
 // }
 
 
-
-displayAllBooks()
+UI.setUpForm()
+UI.displayAllBooks()
 
 
 
